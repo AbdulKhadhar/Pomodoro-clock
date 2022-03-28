aff1027153702e1ced0589c3ed5d6289eb28d890
AbdulKhadhar <abdulkhadhar13@gmail.com>
2022-03-28 22:11:27 +0530

    Edit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          25+5 <code>Pomodoro Clock</code> On progress !
         </p>
         <a
           className="App-link"
